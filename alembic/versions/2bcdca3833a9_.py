70113b84dd46d9b9bc2f4fc89bc5e647dc989bab
sonnygithub2017 <shinengl@gmail.com>
2023-06-04 16:03:05 -0700

    remove product table that out of project

--- alembic/versions/2bcdca3833a9_.py
+++ alembic/versions/2bcdca3833a9_.py
@@ -25,7 +25,7 @@ def upgrade() -> None:
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('post_id', 'user_id')
     )
-    op.drop_table('product')
+    # op.drop_table('product')
     op.add_column('posts', sa.Column('published', sa.Boolean(), server_default='True', nullable=False))
     op.add_column('posts', sa.Column('create_at', sa.TIMESTAMP(timezone=True), server_default=sa.text('now()'), nullable=False))
     op.alter_column('posts', 'content',
@@ -51,14 +51,14 @@ def downgrade() -> None:
                nullable=True)
     op.drop_column('posts', 'create_at')
     op.drop_column('posts', 'published')
-    op.create_table('product',
-    sa.Column('name', sa.VARCHAR(), autoincrement=False, nullable=False),
-    sa.Column('price', sa.INTEGER(), autoincrement=False, nullable=False),
-    sa.Column('id', sa.INTEGER(), autoincrement=True, nullable=False),
-    sa.Column('is_sale', sa.BOOLEAN(), server_default=sa.text('false'), autoincrement=False, nullable=True),
-    sa.Column('inventory', sa.INTEGER(), server_default=sa.text('0'), autoincrement=False, nullable=False),
-    sa.Column('create_at', postgresql.TIME(timezone=True), server_default=sa.text('now()'), autoincrement=False, nullable=False),
-    sa.PrimaryKeyConstraint('id', name='product_pkey')
-    )
+    # op.create_table('product',
+    # sa.Column('name', sa.VARCHAR(), autoincrement=False, nullable=False),
+    # sa.Column('price', sa.INTEGER(), autoincrement=False, nullable=False),
+    # sa.Column('id', sa.INTEGER(), autoincrement=True, nullable=False),
+    # sa.Column('is_sale', sa.BOOLEAN(), server_default=sa.text('false'), autoincrement=False, nullable=True),
+    # sa.Column('inventory', sa.INTEGER(), server_default=sa.text('0'), autoincrement=False, nullable=False),
+    # sa.Column('create_at', postgresql.TIME(timezone=True), server_default=sa.text('now()'), autoincrement=False, nullable=False),
+    # sa.PrimaryKeyConstraint('id', name='product_pkey')
+    # )
     op.drop_table('votes')
     # ### end Alembic commands ###
