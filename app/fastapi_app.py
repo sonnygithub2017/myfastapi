f796315cbcf64e8f49105c1d372a7dd1a9581265
sonnygithub2017 <shinengl@gmail.com>
2023-06-07 22:00:37 -0700

    modify hello world

--- app/fastapi_app.py
+++ app/fastapi_app.py
@@ -28,7 +28,7 @@ app.add_middleware(
 
 @app.get("/")
 async def root():
-  return {"message": "Hello World"}
+  return {"message": "Hello World !!!!!!"}
 
 app.include_router(post.router)
 app.include_router(user.router)
